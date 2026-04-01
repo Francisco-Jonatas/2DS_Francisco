5f4c23c467d2d50d0941010faa7f0ba45bfe79f5
Francisco-Jonatas <joninha324@gmail.com>
2026-04-01 16:44:08 -0300

    Add files via upload

---
+++ ex4/Media.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    internal class Media
+    {
+        public double Calculo()
+        {
+            Console.WriteLine("Insira uma nota");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira uma nota");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira uma nota");
+            double num3 = double.Parse(Console.ReadLine());
+            double resultado = (num1 + num2 + num3) / 3;
+            if (resultado >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+            }
+
+            return resultado;
+        }
+    }
+}
