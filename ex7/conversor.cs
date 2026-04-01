eefc153a877317238ca522bf3409b3f69602e8bf
Francisco-Jonatas <joninha324@gmail.com>
2026-04-01 16:45:43 -0300

    Add files via upload

---
+++ ex7/conversor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    internal class conversor
+    {
+        public void Converter()
+        {
+            Console.Write("Digite o valor em dólares: ");
+            double dolar = double.Parse(Console.ReadLine());
+            double taxa = 5.4f;
+            double real = dolar * taxa;
+            Console.WriteLine("Valor em reais R$:" +real);
+        }
+    }
+}
